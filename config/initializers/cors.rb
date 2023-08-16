7295913d7f01ae8d0b5fcc6a7bf28c1d141d3c69
Metaverse-Nabeel <nabeelahmed3129@gmail.com>
2023-08-16 23:12:21 +0500

    Fix linter errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,10 +7,10 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173"
+    origins 'http://localhost:5173'
 
-    resource "/api/v1/*",
-      headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '/api/v1/*',
+             headers: :any,
+             methods: %i[get post put patch delete options head]
   end
 end
